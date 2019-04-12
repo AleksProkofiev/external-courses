2f1d0d94c301fbc9f91ae8e0aeceb1622d59902c
AleksProkofiev <aleksdfr@gmail.com>
2019-04-12 10:43:54 +0300

    Created a calculator that can perform addition, subtraction, division and multiplication operations.

---
+++ src/ex5_js-functions/task.js
@@ -0,0 +1,50 @@
+function Calc(){
+  var currentValue = 0;
+
+  function add(value){
+    if(isNumeric(value)){
+      currentValue += value;
+    }
+    return add;
+  }
+
+  function subtract(value){
+    if(isNumeric(value)){
+      currentValue -= value;
+    }
+    return subtract;
+  }
+
+  function divide(value){
+    if(isNumeric(value)){
+      currentValue /= value;
+    }
+    return divide;
+  }
+
+  function multiply(value){
+    if(isNumeric(value)){
+      currentValue *= value;
+    }
+    return multiply;
+  }
+
+  function getResult(){
+    return currentValue;
+  }
+
+  function reset(){
+    currentValue = 0;
+  }
+
+  return {add, subtract, divide, multiply, getResult, reset}
+};
+
+function isNumeric(value) {
+  return !isNaN(parseFloat(value)) && isFinite(value);
+};
+
+var Calculator = Calc();
+
+module.exports = Calculator;
+
