95d0327979926fc8cd3674ef2d9121148e800e87
AleksProkofiev <aleksdfr@gmail.com>
2019-04-29 21:41:00 +0300

    task_02.js  - changed the structure of the code (method Array.from is used to get an array from a collection of elements);

--- src/ex12_html_css_js/task_01.js
+++ src/ex12_html_css_js/task_01.js
@@ -3,7 +3,7 @@ window.addEventListener("resize", setElementDimensions);
 function setElementDimensions() {
   var width = document.documentElement.clientWidth;
   var height = document.documentElement.clientHeight;
-  var block = document.getElementById("container");
+  var block = document.getElementsByTagName("article")[0];
   block.style.width = (width / 50) +"%";
   block.style.height = (height / 50) +"%";
 }
--- src/ex12_html_css_js/task_02.js
+++ src/ex12_html_css_js/task_02.js
@@ -1,23 +1,23 @@
 var topMenu = document.getElementById("top-menu");
-var menuItems = document.getElementsByClassName("menu-item");
-Array.from(menuItems, function (elem) {
-  elem.addEventListener("mouseover", setBg);
-  elem.addEventListener("mouseout", setBg);
-});
 topMenu.addEventListener("click", showItems);
 topMenu.addEventListener("mouseover", showShadow);
 topMenu.addEventListener("mouseout", showShadow);
-  function setBg() {
-    this.classList.toggle("blueBg")
-  }
-  function showItems() {
-    Array.from(menuItems,function (elem) {
-      elem.classList.toggle("hide_elem")
-    })
-  }
-  function showShadow() {
-    topMenu.classList.toggle("shadow");
-  }
+var menuItemsArray = Array.from(document.getElementsByClassName("menu-item"));
+menuItemsArray.forEach(function (elem) {
+    elem.addEventListener("mouseover", setBg);
+    elem.addEventListener("mouseout", setBg);
+});
+function setBg() {
+  this.classList.toggle("blueBg")
+}
+function showItems() {
+  menuItemsArray.forEach(function (elem) {
+    elem.classList.toggle("hide_elem")
+  })
+}
+function showShadow() {
+  topMenu.classList.toggle("shadow");
+}
 
 
 
